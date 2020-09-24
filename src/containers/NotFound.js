efc0912dd1eeafe74f7c9640088acc7470e18323
Petros Tsavdaridis <peteropon@gmail.com>
2020-09-24 22:32:15 +0200

    Added NotFound page.

---
+++ src/containers/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./NotFound.css";
+
+export default function NotFound() {
+  return (
+    <div className="NotFound">
+      <h3>
+        Sorry, page not found! <br /> Or you might just destroyed internet...
+      </h3>
+    </div>
+  );
+}
