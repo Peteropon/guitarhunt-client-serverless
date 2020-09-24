7d103ae6c2db9a92413a206be3b0479537617e8f
Petros Tsavdaridis <peteropon@gmail.com>
2020-09-24 22:32:39 +0200

    Set up logging in and out.

---
+++ src/config.js
@@ -0,0 +1,18 @@
+export default {
+  STRIPE_KEY:
+    "sk_test_51HAjXmFeB5e0LAgRHgp7ZzgdqxP0GDvkZ5YWXLycSQAryXFWwwBcKPqIZqWSeHcqu6h9lh3hu6L2xojJ6PS9DWEK00Sq538cEq",
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "auktionera-2-api-dev-attachmentsbucket-fiopbngskbuh",
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://qfyfbnnxx4.execute-api.us-east-2.amazonaws.com/dev",
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_9zczYu0W6",
+    APP_CLIENT_ID: "76brgafkqme201jqkaqeqq9299",
+    IDENTITY_POOL_ID: "us-east-2:d4893808-eb80-49a9-8e61-f57d6ea12082",
+  },
+};
