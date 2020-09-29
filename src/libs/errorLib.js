d81e75d4483714b0f474a13e0deeff69e5c1ff87
Petros Tsavdaridis <peteropon@gmail.com>
2020-09-29 14:16:30 +0200

    Installed and setup Sentry.

--- src/libs/errorLib.js
+++ src/libs/errorLib.js
@@ -1,4 +1,29 @@
 import { toast } from "react-toastify";
+import * as Sentry from "@sentry/browser";
+
+const isLocal = process.env.NODE_ENV === "development";
+
+export function initSentry() {
+  if (isLocal) {
+    return;
+  }
+
+  Sentry.init({
+    dsn:
+      "https://b7c372b2dd774eb8b8c0b42ad1a07a6c@o454627.ingest.sentry.io/5444909",
+  });
+}
+
+export function logError(error, errorInfo = null) {
+  if (isLocal) {
+    return;
+  }
+
+  Sentry.withScope((scope) => {
+    errorInfo && scope.setExtras(errorInfo);
+    Sentry.captureException(error);
+  });
+}
 
 export function onError(error) {
   let message = error.toString();
