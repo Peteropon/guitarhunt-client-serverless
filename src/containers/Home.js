fcb5218c8e86c201ce2a46e175bde9f9b104c69a
Petros Tsavdaridis <peteropon@gmail.com>
2020-11-05 21:38:14 +0100

    Updated home to display guitars by vote

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { ListGroup, ListGroupItem } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
+import { ListGroup } from "react-bootstrap";
 import { useAppContext } from "../libs/contextLib";
 import { onError } from "../libs/errorLib";
 import { API } from "aws-amplify";
 import Spinner from "../components/Spinner";
 import "./Home.css";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   const [guitars, setGuitars] = useState([]);
@@ -32,54 +32,43 @@ export default function Home() {
   }, [isAuthenticated]);
 
   function loadGuitars() {
-    return API.get("guitars", "/guitars");
+    return API.get("guitars", "/rating");
   }
 
   function renderGuitarsList(guitars) {
-    return [{}].concat(guitars).map((guitar, i) =>
-      i !== 0 ? (
-        <LinkContainer key={guitar.guitarId} to={`/guitars/${guitar.guitarId}`}>
-          <ListGroupItem header={guitar.title.trim().split("\n")[0]}>
-            {"Created: " + new Date(guitar.createdAt).toLocaleString()}
-          </ListGroupItem>
-        </LinkContainer>
-      ) : (
-        <LinkContainer key="new" to="/guitars/new">
-          <ListGroupItem>
-            <h4>
-              <b>{"\uFF0B"}</b> Create a new guitar
-            </h4>
-          </ListGroupItem>
-        </LinkContainer>
-      )
-    );
-  }
-
-  function renderLander() {
-    return (
-      <div className="lander">
-        <h1>Guitarera</h1>
-        <p>A good place for guitars</p>
-      </div>
-    );
-  }
-
-  function renderGuitars() {
-    return (
-      <div className="guitars">
-        <h2>Your guitars</h2>
-        {isLoading ? (
-          <Spinner />
-        ) : (
-          <ListGroup>{!isLoading && renderGuitarsList(guitars)}</ListGroup>
-        )}
-      </div>
-    );
+    return guitars.map((guitar) => {
+      return (
+        <ListGroup.Item
+          key={guitar.guitarId}
+          action
+          as={Link}
+          to={`/guitars/${guitar.guitarId}`}
+        >
+          <p>{guitar.title}</p>
+          {"Votes: " + guitar.votes}
+        </ListGroup.Item>
+      );
+    });
   }
 
   return (
     <div className="Home">
-      {isAuthenticated ? renderGuitars() : renderLander()}
+      <div className="lander">
+        <h1>Guitar Hunt</h1>
+        <p>A good place to rate guitars</p>
+      </div>
+      {isAuthenticated ? (
+        <div className="guitars">
+          <h2>Top guitars</h2>
+          {isLoading ? (
+            <Spinner />
+          ) : (
+            <ListGroup>{!isLoading && renderGuitarsList(guitars)}</ListGroup>
+          )}
+        </div>
+      ) : (
+        <p>Sign up or login in order to start viewing and rating guitars.</p>
+      )}
     </div>
   );
 }
