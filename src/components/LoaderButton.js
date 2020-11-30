f8279c94e0a94a783f43ef5a19c277aa9861f957
Petros Tsavdaridis <peteropon@gmail.com>
2020-11-30 13:20:47 +0100

    Installed react icons.

--- src/components/LoaderButton.js
+++ src/components/LoaderButton.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import { BsArrowRepeat } from "react-icons/bs";
 import "./LoaderButton.css";
 
 export default function LoaderButton({
@@ -14,7 +15,7 @@ export default function LoaderButton({
       disabled={disabled || isLoading}
       {...props}
     >
-      {isLoading}
+      {isLoading && <BsArrowRepeat className="spinning" />}
       {props.children}
     </Button>
   );
