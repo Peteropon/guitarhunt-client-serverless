882b7d832970d0301216c253ae6425188365de97
Petros Tsavdaridis <peteropon@gmail.com>
2020-12-05 14:44:08 +0100

    Restructured and improved the footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,28 @@
 import React from "react";
 import "./Footer.css";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { Col, Container, Row } from "react-bootstrap";
 
 export default function Footer() {
   return (
-    <div className="footer">
-      <p>
-        <a href="https://github.com/Peteropon">
+    <Container fluid as="footer">
+      <Row xs={2}>
+        <Col sm={{ order: 1 }} as="a" href="https://github.com/Peteropon">
           <FaGithub size="2em" />
-        </a>
-        Made by Petros
-        <a href="https://www.linkedin.com/in/petros-tsavdaridis-a8575059/">
+        </Col>
+        <Col
+          sm={{ order: 3 }}
+          as="a"
+          href="https://www.linkedin.com/in/petros-tsavdaridis-a8575059/"
+        >
           <FaLinkedin size="2em" />
-        </a>
-      </p>
-    </div>
+        </Col>
+        <Col xs={12} sm={{ order: 2 }} as="p">
+          Made by Petros <br /> For educational and demonstrative puproses only{" "}
+          <br />
+          &copy; PT 2020
+        </Col>
+      </Row>
+    </Container>
   );
 }
